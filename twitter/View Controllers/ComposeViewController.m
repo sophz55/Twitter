3f62e30c2a98447620f463710c2f7586aeda92f9
Sophia Zheng <szheng@fb.com>
2018-07-06 17:07:04 -0700

    setting character limit to 280 when composing tweet

--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -12,6 +12,7 @@
 @interface ComposeViewController () <UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextView *statusContentTextView;
+@property (weak, nonatomic) IBOutlet UILabel *charactersRemainingLabel;
 
 @end
 
@@ -20,7 +21,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.statusContentTextView.delegate = self;
-//    [self textViewDidBeginEditing:self.statusContentTextView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,6 +31,28 @@
 - (void)textViewDidBeginEditing:(UITextView *)textView {
     textView.text = @"";
     textView.textColor = [UIColor blackColor];
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    long characterLimit = 280;
+    NSString *newText = [self.statusContentTextView.text stringByReplacingCharactersInRange:range withString:text];
+    
+    long charactersRemaining = characterLimit - newText.length;
+    if (charactersRemaining <= 5) {
+        self.charactersRemainingLabel.textColor = [UIColor redColor];
+    } else {
+        self.charactersRemainingLabel.textColor = [UIColor blackColor];
+    }
+    if (newText.length < characterLimit - 1) {
+        self.charactersRemainingLabel.text = [NSString stringWithFormat:@"%ld characters remaining", charactersRemaining];
+        NSLog(@"%ld", newText.length);
+    } else if (newText.length == characterLimit - 1) {
+        self.charactersRemainingLabel.text = @"1 character remaining";
+    } else {
+        self.charactersRemainingLabel.text = @"0 characters remaining";
+    }
+    
+    return newText.length <= characterLimit;
 }
 
 - (IBAction)didTapClose:(id)sender {
